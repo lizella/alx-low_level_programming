07234cb79ad3829a5bcb5e72857e14605617b8bd
Liz Kilonzo <lizkilonzo7@gmail.com>
2023-02-22 06:43:14 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,14 @@
  * _isalpha - checks for alphabets
  *  @c: character to be checked
  *
- *  Return: 1 if character is a letter,0 otherwise
+ *  Return: 1 if character is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
-			return (1);
-			else
-			return (0);
+return (1);
+else
+return (0);
 }
 
 
